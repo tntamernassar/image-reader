cc965f2c8ce7c73c896bb509b3b95990813dd8d7
Tamer Nassar <tamerna@post.bgu.ac.il>
2018-07-14 14:05:32 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,19 +1,17 @@
 <?php
+
 include "connection.php";
 
-
 if(isset($_POST['postimagecode'])){
-	$imagecode = $_POST['postimagecode'];
-$width = $_POST['width'];
-$height = $_POST['height'];
-
-$query = mysql_query("INSERT INTO images VALUES('','$imagecode','$width','$height')");
-
-echo $imagecode;
-}
-else{
 	
-	echo "No Post request found :)";
+   $imagecode = $_POST['postimagecode'];
+   $width = $_POST['width'];
+   $height = $_POST['height'];
+   $query = mysql_query("INSERT INTO images VALUES('','$imagecode','$width','$height')");
+   echo $imagecode;
 	
 }
+else
+   echo "No Post request found :)";
+
 ?>
